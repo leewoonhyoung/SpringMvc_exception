949f50943772f97736bbdb1c28ba843a50312784
leewoonhyoung <wh3619@gmail.com>
2022-03-02 20:47:06 +0900

    /* 2022-03-02 api exception + html */

--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -4,9 +4,11 @@ import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 
 // 넘어와서 보니까 ErrorPage RuntimeException이 존재한다 그럼 /error-page/500 경로에 있는 errorpagecontroller를 호출해
 //오류 페이지 등록
+@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
--- src/main/java/hello/exception/servlet/ErrorPageController.java
+++ src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,11 +1,16 @@
 package hello.exception.servlet;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -36,6 +41,12 @@ public class ErrorPageController {
     public String errorPage500(HttpServletRequest request, HttpServletResponse response){
         log.info("errorPage 500");
         return "error-page/500";
+    }
+    @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> errorPage500Api(HttpServletRequest request, HttpServletResponse response){
+        log.info("API errorPage 500");
+
+        Map<String, Object> result = new HashMap<>();
     }
 
 

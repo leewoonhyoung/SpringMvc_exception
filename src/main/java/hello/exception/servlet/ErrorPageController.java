6f66a61406321246e67fd385fd9be653b1bf4a39
leewoonhyoung <wh3619@gmail.com>
2022-04-07 17:14:49 +0900

    /* 2022-04-07  spring @ExceptionHandler 총정리 + 주석

--- src/main/java/hello/exception/servlet/ErrorPageController.java
+++ src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -20,15 +20,6 @@ import java.util.Map;
 // error-page/500관련된 error-page/500 view를 호출해줘~
 public class ErrorPageController {
 
-    //RequestDispatcher 상수로 정의되어 있음
-    public static final String ERROR_EXCEPTION = "javax.servlet.error.exception"; // 예외
-    public static final String ERROR_EXCEPTION_TYPE = "javax.servlet.error.exception_type"; // 예외 타입
-    public static final String ERROR_MESSAGE = "javax.servlet.error.message"; // 오류 메세지
-    public static final String ERROR_REQUEST_URI = "javax.servlet.error.request_uri"; // 클라이언트 요청 URI
-    public static final String ERROR_SERVLET_NAME = "javax.servlet.error.servlet_name"; // 오류가 발생한 서블릿 이름
-    public static final String ERROR_STATUS_CODE = "javax.servlet.error.status_code"; // HTTP   상태 코드
-
-
     // static  오류//
     @RequestMapping("/error-page/404")
     public String errorPage404(HttpServletRequest request, HttpServletResponse response){
@@ -40,6 +31,26 @@ public class ErrorPageController {
     public String errorPage500(HttpServletRequest request, HttpServletResponse response){
         log.info("errorPage 500");
         return "error-page/500";
+    }
+
+    //RequestDispatcher 상수로 정의되어 있음
+    public static final String ERROR_EXCEPTION = "javax.servlet.error.exception"; // 예외
+    public static final String ERROR_EXCEPTION_TYPE = "javax.servlet.error.exception_type"; // 예외 타입
+    public static final String ERROR_MESSAGE = "javax.servlet.error.message"; // 오류 메세지
+    public static final String ERROR_REQUEST_URI = "javax.servlet.error.request_uri"; // 클라이언트 요청 URI
+    public static final String ERROR_SERVLET_NAME = "javax.servlet.error.servlet_name"; // 오류가 발생한 서블릿 이름
+    public static final String ERROR_STATUS_CODE = "javax.servlet.error.status_code"; // HTTP   상태 코드
+
+
+
+    private void printErrorInfo(HttpServletRequest request) {
+        log.info("ERROR_EXCEPTION: ex=", request.getAttribute(ERROR_EXCEPTION));
+        log.info("ERROR_EXCEPTION_TYPE: {}", request.getAttribute(ERROR_EXCEPTION_TYPE));
+        log.info("ERROR_MESSAGE: {}", request.getAttribute(ERROR_MESSAGE)); //ex의 경우 NestedServletException 스프링이 한번 감싸서 반환
+        log.info("ERROR_REQUEST_URI: {}", request.getAttribute(ERROR_REQUEST_URI));
+        log.info("ERROR_SERVLET_NAME: {}", request.getAttribute(ERROR_SERVLET_NAME));
+        log.info("ERROR_STATUS_CODE: {}", request.getAttribute(ERROR_STATUS_CODE));
+        log.info("dispatchType={}", request.getDispatcherType());
     }
 
     // Json api 오류 page 추가//
@@ -58,16 +69,6 @@ public class ErrorPageController {
 
         Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
-    }
-
-    private void printErrorInfo(HttpServletRequest request) {
-        log.info("ERROR_EXCEPTION: ex=", request.getAttribute(ERROR_EXCEPTION));
-        log.info("ERROR_EXCEPTION_TYPE: {}", request.getAttribute(ERROR_EXCEPTION_TYPE));
-        log.info("ERROR_MESSAGE: {}", request.getAttribute(ERROR_MESSAGE)); //ex의 경우 NestedServletException 스프링이 한번 감싸서 반환
-        log.info("ERROR_REQUEST_URI: {}", request.getAttribute(ERROR_REQUEST_URI));
-        log.info("ERROR_SERVLET_NAME: {}", request.getAttribute(ERROR_SERVLET_NAME));
-        log.info("ERROR_STATUS_CODE: {}", request.getAttribute(ERROR_STATUS_CODE));
-        log.info("dispatchType={}", request.getDispatcherType());
     }
 
 
